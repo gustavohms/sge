eae491b9aa5002b1efb576ccd3e96eda413c636a
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-04-20 06:56:55 -0300

    feat: add brand creation functionality with form and template

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
-from . import models
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from . import models, forms
 
 class BrandListView(ListView):
     model = models.Brand
@@ -13,3 +14,9 @@ class BrandListView(ListView):
         if name:
             queryset = queryset.filter(name__icontains=name)
         return queryset
+    
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
