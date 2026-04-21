b4e4389fca1c903f9f3d7f732beb82a7fed7c83a
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-04-21 14:30:59 -0300

    feat: CRUD Brands complete, add edit and delete.

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
 class BrandListView(ListView):
@@ -19,4 +19,19 @@ class BrandCreateView(CreateView):
     model = models.Brand
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
