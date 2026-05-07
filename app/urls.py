60120a55803ec326dea7b2bc5a338164c362debb
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-05-06 22:24:46 -0300

    feat: Add admin in all modules and create CRUD for inflows

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path('', include('brands.urls'))
+    path('', include('brands.urls')),
+    path('', include('categories.urls')),
+    path('', include('suppliers.urls')),
+    path('', include('inflows.urls')),
 ]
