42ab81802ec00f1362fc42f9627a0c4277b97a29
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-05-09 21:38:02 -0300

    feat: Add product management with CRUD operations and templates

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='products')
     description = models.TextField(null=True, blank=True)
     serie_number = models.CharField(max_length=200, null=True, blank=True)
-    cost_prince = models.DecimalField(max_digits=20, decimal_places=2)
+    cost_price = models.DecimalField(max_digits=20, decimal_places=2)
     selling_price = models.DecimalField(max_digits=20, decimal_places=2)
     quantity = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
