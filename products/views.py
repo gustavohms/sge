a4e183d2f2702b70d8f6c87bd72197ca01f9c895
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-05-11 19:17:19 -0300

    feat: Enhance product list with filtering options for title, series number, category, and brand

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
+from categories.models import Category
+from brands.models import Brand
 
 class ProductListView(ListView):
     model = models.Product
@@ -10,11 +12,31 @@ class ProductListView(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        serie_number = self.request.GET.get('serie_number')
         title = self.request.GET.get('title')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
 
         if title:
             queryset = queryset.filter(name__icontains=title)
+
+        if serie_number:
+            queryset = queryset.filter(serie_number__icontains=serie_number)
+        
+        if category:
+            queryset = queryset.filter(category__id=category)
+        
+        if brand:
+            queryset = queryset.filter(brand__id=brand)
+        
+
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
+        return context
     
 class ProductCreateView(CreateView):
     model = models.Product
