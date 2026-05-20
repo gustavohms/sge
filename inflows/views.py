ae3d5b231a1effc8b623d0dd9f74c298448201fd
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2026-05-20 17:50:51 -0300

    feat: Add permission requirements for brand, category, inflow, outflow, product, and supplier views

--- inflows/views.py
+++ inflows/views.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
-class InflowListView(LoginRequiredMixin, ListView):
+class InflowListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = models.Inflow
     template_name = 'inflow_list.html'
     context_object_name = 'inflows'
     paginate_by = 10
+    permission_required = 'inflows.view_inflow'
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -17,12 +18,14 @@ class InflowListView(LoginRequiredMixin, ListView):
             queryset = queryset.filter(product__title__icontains=product)
         return queryset
     
-class InflowCreateView(LoginRequiredMixin, CreateView):
+class InflowCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = models.Inflow
     template_name = 'inflow_create.html'
     form_class = forms.InflowForm
     success_url = reverse_lazy('inflow_list')
+    permission_required = 'inflows.add_inflow'
 
-class InflowDetailView(LoginRequiredMixin, DetailView):
+class InflowDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = models.Inflow
     template_name = 'inflow_detail.html'
+    permission_required = 'inflows.view_inflow'
